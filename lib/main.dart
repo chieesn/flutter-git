7c13c81a48c20c7c4341bfdc9553dba6698ac690
chieesn <85233133+chieesn@users.noreply.github.com>
2024-07-24 17:09:33 +0800

    reply from macos

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times (CHANGING ON LINUX):',
+              'You have pushed the button this many times (CHANGING ON LINUX; copy that Linux from MACOS over):',
             ),
             Text(
               '$_counter',
